926d0323cdc6f96b6e569a05e67386e6cc506c12
lajjo-96 <aprajithabajpai@gmail.com>
2019-02-08 16:12:03 +0530

    Foodie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { FavoriteComponent } from "./favorite/favorite.component";
+import { SearchComponent } from "./search/search.component";
 
-const routes: Routes = [];
 
+const routes: Routes = [ {
+  path: "home", component: HomeComponent
+},
+{
+  path: "favorite", component: FavoriteComponent
+},
+{
+  path: "search/:name", component: SearchComponent
+},
+{
+  path: "**", component: HomeComponent 
+}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule }    from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { HomeComponent } from './home/home.component';
+import { MoviesService } from "./movies.service";
+import { SearchComponent } from './search/search.component'; //JS IMOPRT
+import {FormsModule} from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FavoriteComponent,
+    HomeComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+
   ],
-  providers: [],
+  providers: [MoviesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
